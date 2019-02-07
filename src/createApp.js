211a97be83fd9f59d8be74d29c3b1ec4629b9f95
Aiden Nibali <dismaldenizen@gmail.com>
2019-02-08 09:34:24 +1100

    Add experiment directory watching

--- src/createApp.js
+++ src/createApp.js
@@ -5,59 +5,86 @@ import yaml from 'js-yaml';
 import crypto from 'crypto';
 import csv from 'csv-streamify';
 import base32 from 'base32';
+import chokidar from 'chokidar';
 
 
 const calcHash = str => base32.encode(crypto.createHash('md5').update(str).digest().slice(0, 8));
 
-// Load all experiments from the experiment root directory.
-const loadExperiments = async (rootDir) => {
-  const experimentDirs = fs.readdirSync(rootDir)
-    .map(entry => path.resolve(rootDir, entry))
-    .filter(fullPath => fs.statSync(fullPath).isDirectory());
+const watchExperiments = (rootDir) => {
+  const experimentsState = {
+    simpleTracesById: {},
+    traceDataById: {},
+    manifestsByExperiment: {},
+    readTraceData: function(traceDataId) {
+      const steps = [];
+      const values = [];
+      const parser = csv({ delimiter: '\t' });
+      parser.on('data', ([step, value]) => {
+        steps.push(parseInt(step, 10));
+        values.push(JSON.parse(value));
+      });
+      const promise = new Promise((resolve, reject) => {
+        parser.on('end', () => resolve({ id: traceDataId, steps, values }));
+        parser.on('error', reject);
+      });
+      const { filePath } = experimentsState.traceDataById[traceDataId];
+      fs.createReadStream(filePath).pipe(parser);
+      return promise;
+    },
+  };
 
-  const manifestsByExperiment = {};
+  const reloadExperiments = (events) => {
+    // TODO: Use `events` to prevent having to do a full reload of all experiments.
 
-  experimentDirs.forEach(experimentDir => {
-    const experimentId = path.basename(experimentDir);
-    const yamlText = fs.readFileSync(path.join(experimentDir, 'manifest.yml'));
-    manifestsByExperiment[experimentId] = yaml.safeLoad(yamlText);
-  });
+    const simpleTracesById = {};
+    const traceDataById = {};
+    const manifestsByExperiment = {};
 
-  const simpleTracesById = {};
-  const traceDataById = {};
+    const experimentDirs = fs.readdirSync(rootDir)
+      .map(entry => path.resolve(rootDir, entry))
+      .filter(fullPath => fs.statSync(fullPath).isDirectory());
 
-  const readTraceData = traceDataId => new Promise((resolve, reject) => {
-    const steps = [];
-    const values = [];
-    const parser = csv({ delimiter: '\t' });
-    parser.on('data', ([step, value]) => {
-      steps.push(parseInt(step, 10));
-      values.push(JSON.parse(value));
+    experimentDirs.forEach(experimentDir => {
+      const experimentId = path.basename(experimentDir);
+      const yamlText = fs.readFileSync(path.join(experimentDir, 'manifest.yml'));
+      manifestsByExperiment[experimentId] = yaml.safeLoad(yamlText);
     });
-    parser.on('end', () => resolve({ id: traceDataId, steps, values }));
-    parser.on('error', reject);
-    const { filePath } = traceDataById[traceDataId];
-    fs.createReadStream(filePath).pipe(parser);
-  });
 
-  Object.entries(manifestsByExperiment).forEach(([experimentId, manifest]) => {
-    manifest.traces.simple.forEach(trace => {
-      const traceDataId = calcHash(`${experimentId}/${trace.data}`);
-      traceDataById[traceDataId] = {
-        id: traceDataId,
-        filePath: path.join(rootDir, experimentId, trace.data),
-      };
-      const traceId = calcHash(`${experimentId}/${trace.name}`);
-      simpleTracesById[traceId] = {
-        id: traceId,
-        experiment: experimentId,
-        name: trace.name,
-        traceData: traceDataId,
-      };
+    Object.entries(manifestsByExperiment).forEach(([experimentId, manifest]) => {
+      manifest.traces.simple.forEach(trace => {
+        const traceDataId = calcHash(`${experimentId}/${trace.data}`);
+        traceDataById[traceDataId] = {
+          id: traceDataId,
+          filePath: path.join(rootDir, experimentId, trace.data),
+        };
+        const traceId = calcHash(`${experimentId}/${trace.name}`);
+        simpleTracesById[traceId] = {
+          id: traceId,
+          experiment: experimentId,
+          name: trace.name,
+          traceData: traceDataId,
+        };
+      });
     });
+
+    experimentsState.manifestsByExperiment = manifestsByExperiment;
+    experimentsState.simpleTracesById = simpleTracesById;
+    experimentsState.traceDataById = traceDataById;
+  };
+
+  const events = [];
+  chokidar.watch(rootDir).on('all', (event, fsPath) => {
+    events.push({ event, fsPath });
   });
 
-  return { simpleTracesById, readTraceData, manifestsByExperiment };
+  const interval = 1000; // Minimum time between calls to `reloadExperiments`.
+  setInterval(() => {
+    if(events.length > 0) {
+      reloadExperiments(events.splice(0, events.length));
+    }
+  }, interval);
+
+  return experimentsState;
 };
 
 const renderHtmlPage = (title, assetManifest) => (`
@@ -77,7 +104,7 @@ const renderHtmlPage = (title, assetManifest) => (`
 
 
 export default async (rootDir) => {
-  const { simpleTracesById, readTraceData, manifestsByExperiment } = await loadExperiments(rootDir);
+  const experimentsState = watchExperiments(rootDir);
 
   const app = express();
 
@@ -93,17 +120,17 @@ export default async (rootDir) => {
   });
 
   app.get('/experiments', (req, res) => {
-    res.json(Object.keys(manifestsByExperiment).map(id => ({ id })));
+    res.json(Object.keys(experimentsState.manifestsByExperiment).map(id => ({ id })));
   });
 
   app.get('/experiments/:experimentId/traces', (req, res) => {
-    const traces = Object.values(simpleTracesById)
+    const traces = Object.values(experimentsState.simpleTracesById)
       .filter(trace => trace.experiment === req.params.experimentId);
     res.json(traces);
   });
 
   app.get('/experiments/:experimentId/views', (req, res) => {
-    const manifest = manifestsByExperiment[req.params.experimentId];
+    const manifest = experimentsState.manifestsByExperiment[req.params.experimentId];
     const views = manifest.views.map(view => ({
       id: calcHash(`${req.params.experimentId}/${view.name}`),
       experiment: req.params.experimentId,
@@ -113,7 +140,7 @@ export default async (rootDir) => {
   });
 
   app.get('/tracedata/:traceDataId', (req, res) => {
-    readTraceData(req.params.traceDataId).then(traceData => {
+    experimentsState.readTraceData(req.params.traceDataId).then(traceData => {
       res.json(traceData);
     });
   });
